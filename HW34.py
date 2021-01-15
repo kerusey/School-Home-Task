c9e8642c9b5cc735343935df74dcb6f3598cd14d
kerusey <38439184+kerusey@users.noreply.github.com>
2021-01-15 13:12:44 +0300

    Update HW34.py

--- HW34.py
+++ HW34.py
@@ -4,4 +4,13 @@ cashe = [{len(divisors(i)): i} for i in range(586132, 586430)]
 
 cashe.sort(key = lambda item: list(item.keys())[0])
 
-print(cashe) 
+majorNumberDivisors = list(cashe[-3].keys())[0]
+majorNumber = cashe[-3][majorNumberDivisors]
+
+
+minorNumberDivisors = list(cashe[-1].keys())[0]
+minorNumber = cashe[-1][minorNumberDivisors]
+
+print(majorNumberDivisors, majorNumber, majorNumber // divisors(majorNumber)[1])
+
+print(minorNumberDivisors, minorNumber, minorNumber // divisors(minorNumber)[1])
