64a4a9ba287cf64ae1a57470a852ba571fab7c65
kerusey <38439184+kerusey@users.noreply.github.com>
2021-01-15 12:38:32 +0300

    Update HW22.py

--- HW22.py
+++ HW22.py
@@ -1,7 +1,4 @@
 from sympy import divisors
 for i in range(190201, 190280):
 	if len(list(filter(lambda item: item % 2 == 0, divisors(i)))) == 4:
-		print(i)
-	
-		
-	
+		print([i for i in divisors(i) if i % 2 == 0])
